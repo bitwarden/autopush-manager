93b5f2b893db36928eda5bac58cb0fa5f4b02d8c
Matt Gibson <mgibson@bitwarden.com>
2025-04-02 10:29:58 -0700

    Show case usage of package in a web environment
    In reality, this would either be in an electron application or browser extension. You should always prefer using the browser's inbuilt PushManager.

---
+++ examples/website/build.mjs
@@ -0,0 +1,35 @@
+import { context } from "esbuild";
+import { copy } from "esbuild-plugin-copy";
+
+let ctx = await context({
+    entryPoints: ["src/app.ts"],
+    bundle: true,
+    outdir: "build/src",
+    logLevel: "info",
+    sourcemap: "inline",
+    plugins: [
+        copy({
+            // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+            // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+            resolveFrom: "cwd",
+            assets: {
+                from: ["./index.html"],
+                to: ["build/index.html"],
+            },
+            watch: true,
+        }),
+    ],
+});
+
+if (process.env.NODE_ENV !== "dev") {
+  // minify in production
+  await ctx.rebuild({
+    minify: true,
+  });
+} else {
+  await ctx.watch();
+  
+  await ctx.serve({
+      servedir: "build",
+  });
+}
---
+++ examples/website/src/listen.ts
@@ -0,0 +1,23 @@
+import { createPushManager, Logger, StorageInterface } from "autopush-manager";
+
+export class Listener {
+  constructor(readonly storage: StorageInterface, readonly logger: Logger) {}
+
+  async listen(applicationServerKey: string): Promise<void> {
+    this.logger.info("Starting to listen...");
+
+    const pushManager = await createPushManager(this.storage, this.logger);
+    const subscription = await pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey,
+    });
+
+    this.logger.info("Subscription created", JSON.parse(JSON.stringify(subscription)));
+    subscription.addEventListener("notification", (event) => {
+      this.logger.info("Received notification", event);
+    });
+    subscription.addEventListener("pushsubscriptionchange", (event) => {
+      this.logger.info("Subscription changed. I should tell the server!!", event);
+    });
+  }
+}
