82e8f22d2b013d4541098ad3eda0c863711914cc
Matt Gibson <mgibson@bitwarden.com>
2025-04-08 04:45:05 -0700

    Expand-examples (#25)
    * Clean up unused code and enable browser env
    
    * Add more complex command line example
    
    This one allows listening like the simple_listen example, but also provides a way to send messages
    
    * Show case usage of package in a web environment
    
    In reality, this would either be in an electron application or browser extension. You should always prefer using the browser's inbuilt PushManager.
    
    * Add Readme documentation
    
    * `npm run lint:fix && npm run prettier` :robot:
    
    * formatting rules for modules and common scripts

--- examples/simple_listener/listen.cjs
+++ examples/simple_listener/listen.cjs
@@ -1,6 +1,8 @@
 // VAPID keys for example:
 // Public Key: BCh0IFsAIOnY4b_wzbIJjFianmWZ8o0CIYWGiuRHIlEbFJ2doSdk5UlB1iRH4oW7FYi8sv51ZxASxBtpYVBBaUE
 // Private Key: Lf2uqdxcKNy1PiJ5q-o18uA59_61v7a8R9N9j2fWlr0
+const applicationServerKey =
+    "BCh0IFsAIOnY4b_wzbIJjFianmWZ8o0CIYWGiuRHIlEbFJ2doSdk5UlB1iRH4oW7FYi8sv51ZxASxBtpYVBBaUE";
 
 const fs = require("fs");
 
@@ -38,8 +40,7 @@ require("../../build/src/index")
     .then(async (pushManager) => {
         const subscription = await pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey:
-                "BCh0IFsAIOnY4b_wzbIJjFianmWZ8o0CIYWGiuRHIlEbFJ2doSdk5UlB1iRH4oW7FYi8sv51ZxASxBtpYVBBaUE",
+            applicationServerKey,
         });
 
         logger.log("Subscription created", JSON.stringify(subscription));
